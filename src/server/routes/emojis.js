83baa100d9acfc4954aa3f2c9c2170c420a99657
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-09-07 23:08:23 -0600

    added post route you can now post emojis

--- src/server/routes/emojis.js
+++ src/server/routes/emojis.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const knex = require('../db/knex');
-// const validations = require('./validations');
+const validations = require('./validations');
 
 router.get('/', (req, res, next) => {
   knex('emojis')
@@ -10,6 +10,26 @@ router.get('/', (req, res, next) => {
     const renderObject = {};
     renderObject.emojis = emojis;
     res.render('emojis', renderObject);
+  })
+  .catch((error) => {
+    return next(error);
+  });
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('new');
+});
+
+router.post('/new', validations.verify, (req, res, next) => {
+  const emoji = req.body.emoji;
+  const emotion = req.body.emotion;
+
+  knex('emojis').insert({
+    emoji: emoji,
+    emotion: emotion
+  })
+  .then((emoji) => {
+    res.redirect('/emojis');
   })
   .catch((error) => {
     return next(error);
---
+++ src/server/routes/validations.js
@@ -0,0 +1,93 @@
+const knex = require('../db/knex');
+
+function verify(req, res, next) {
+  // container for all errors
+
+  const errors = [];
+
+  // grab values from req.body (form)
+  const emoji = req.body.emoji;
+  const emotion = req.body.emotion;
+
+  isUnique('emoji', emoji, function(error, response) {
+    if (error) {
+      return next(error);
+    }
+    if (response) {
+      errors.push('Emoji already taken 😢');
+    }
+
+    // is emoji blank
+    if (emoji === '') {
+      errors.push('Emoji cannot be blank 😑');
+    }
+
+    // is emotion blank
+    if (emotion === '') {
+      errors.push('Emotion cannot be blank 😑');
+    }
+
+    // send back errors if applicable
+    if (errors.length) {
+      // re-render page if there are errors
+      const renderObject = {};
+      renderObject.errors = errors;
+      return res.render('new', renderObject);
+    } else {
+      // send to next middleware
+      return next();
+    }
+  });
+}
+
+function verifyEdit(req, res, next) {
+  // container for all errors
+
+  const errors = [];
+
+  // grab values from req.body (form)
+  const emoji = req.body.emoji;
+  const emotion = req.body.emotion;
+
+  // is emoji blank
+  if (emoji === '') {
+    errors.push('Emoji cannot be blank 😑');
+  }
+
+  // is emotion blank
+  if (emotion === '') {
+    errors.push('Emotion cannot be blank 😑');
+  }
+
+  // send back errors if applicable
+  if (errors.length) {
+    // re-render page if there are errors
+    const renderObject = {};
+    renderObject.errors = errors;
+    return res.render('emoji', renderObject);
+  } else {
+    // send to next middleware
+    return next();
+  }
+}
+
+// helper function
+
+function isUnique(column, value, callback) {
+  knex('emojis').select().where(column, value)
+  .then((value) => {
+    if (value.length) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  })
+  .catch((error) => {
+    callback(error);
+  });
+}
+
+module.exports = {
+  verify,
+  verifyEdit
+};
